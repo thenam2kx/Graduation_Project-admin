b0acba74cd2fafb44865d2a3848269cb495f72f0
thenam2kx <thenam2kx@gmail.com>
2025-05-05 09:59:20 +0700

    update base admin

--- src/pages/not-found.page.tsx
+++ src/pages/not-found.page.tsx
@@ -1,8 +1,19 @@
+import { Button, Result } from 'antd'
+import { useNavigate } from 'react-router'
 
 
 const NotFoundPage = () => {
+  const navigate = useNavigate()
+
   return (
-    <div>NotFoundPage</div>
+    <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={<Button type="primary" onClick={() => navigate('/')}>Back Home</Button>}
+      />
+    </div>
   )
 }
 
