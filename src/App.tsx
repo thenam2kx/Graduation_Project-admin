c8e6ce913f1cb253df0a548fc7bc049cd8bf3686
thenam2kx <thenam2kx@gmail.com>
2025-06-04 19:25:42 +0700

    fix(auth): fix flick ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
 import { ConfigProvider, theme as antTheme } from 'antd'
 import Routers from './routers/routers'
 import { useAppSelector } from './redux/hooks'
-import { lazy, Suspense } from 'react'
+import { lazy, Suspense, useEffect } from 'react'
 import { useNavigate } from 'react-router'
-import { useQuery } from '@tanstack/react-query'
 import { fetchAccountAPI } from './services/user-service/user.apis'
 
 const MediaModal = lazy(() => import('./components/media-modal/media.modal'))
 
 
 const App = () => {
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
 
   const themeMode = useAppSelector(state => state.app.themeMode)
-  // const { data: infoUser } = useQuery({
-  //   queryKey: ['homeData'],
-  //   queryFn: async () => {
-  //     const res = await fetchAccountAPI()
-  //     return res.data
-  //   },
-  //   refetchOnWindowFocus: false
-  // })
-  // if (!infoUser) {
-  //   navigate('/signin')
-  // }
+  useEffect(() => {
+    (async () => {
+      try {
+        await fetchAccountAPI()
+      } catch (error) {
+        console.error('Error fetching account data:', error)
+        navigate('/signin')
+      }
+    })()
+  }, [navigate])
 
   return (
     <div>
